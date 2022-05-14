cbed62f68f6be054a798b85ea3a5518a2a657dce
Remco Stoeten <remcostoeten@hotmail.com>
2022-05-15 00:40:46 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import {Button, Stack } from "react-bootstrap" 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from "react-bootstrap";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+        <Container className="my-4">
+            <Stack direction="horizontal" gap="2" className="mb-4">
+                <h1 className="me-auto">budget</h1>
+                <Button variant="primary">Add Budget</Button>
+                <Button variant="outline-primary">Add expense</Button>
+            </Stack>
+            <div className="card-container">
+
+            </div>
+        </Container>
+    </>
+    );
 }
 
 export default App;
---
+++ src/components/BudgetCard.js
@@ -0,0 +1,14 @@
+import { Card } from "react-bootstrap";
+
+export default function BudgetCard( {name, amount, max} ) {
+  return (
+        <Card>
+            <Card.Body>
+                <Card.Title>
+                <div>{name}</div>
+                <div>{amount} / {max}</div>
+                </Card.Title>
+            </Card.Body>
+        </Card>
+    )
+}
